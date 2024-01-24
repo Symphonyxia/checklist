a6b0adf4562f02c224b5ae4fa43e827dec4dc6e8
Mary Hope Tabunlupa <115769573+Symphonyxia@users.noreply.github.com>
2024-01-24 11:41:51 +0800

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -10,17 +10,14 @@
 
 <div class="container-fluid ">
 
-    <!-- Page Heading -->
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
         <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
 
     </div>
 
-    <!-- Content Row -->
     <div class="row mx-auto ">
 
-        <!-- Earnings (Monthly) Card Example -->
-        <div class="col-xl-2 col-md-4 mb-4 ">
+        <div class="col-xl-3 col-md-4 mb-4 ">
             <a href="view_questions.php" class="card-link">
                 <div class="card border-left-primary shadow h-100 py-2">
                     <div class="card-body">
@@ -38,8 +35,7 @@
             </a>
         </div>
 
-        <!-- Earnings (Monthly) Card Example -->
-        <div class="col-xl-2 col-md-4 mb-4">
+        <div class="col-xl-3 col-md-4 mb-4">
             <a href="eval_page.php" class="card-link">
                 <div class="card border-left-success shadow h-100 py-2">
                     <div class="card-body">
@@ -57,8 +53,7 @@
             </a>
         </div>
 
-        <!-- Earnings (Monthly) Card Example -->
-        <div class="col-xl-2 col-md-4 mb-4">
+        <div class="col-xl-3 col-md-4 mb-4">
             <a href="create.php" class="card-link">
                 <div class="card border-left-info shadow h-100 py-2">
                     <div class="card-body">
@@ -82,8 +77,7 @@
         </div>
 
 
-        <!-- Pending Requests Card Example -->
-        <div class="col-xl-2 col-md-4 mb-4">
+        <div class="col-xl-3 col-md-4 mb-4">
             <a href="edit_questions.php" class="card-link">
                 <div class="card border-left-warning shadow h-100 py-2">
                     <div class="card-body">
@@ -104,7 +98,7 @@
             </a>
         </div>
 
-        <div class="col-xl-2 col-md-4 mb-4">
+        <div class="col-xl-3 col-md-4 mb-4">
             <a href="records.php" class="card-link">
                 <div class="card border-left-warning shadow h-100 py-2">
                     <div class="card-body">
