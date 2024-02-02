e6ce71b7927a4f5d9ca1122bd84ce2e1e267a769
Mary Hope Tabunlupa <115769573+Symphonyxia@users.noreply.github.com>
2024-02-02 11:04:03 +0800

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -1,6 +1,6 @@
 <link rel="stylesheet" type="text/css" href="resources/fontawesome-free-6.5.1-web/css/fontawesome.css">
 <link rel="stylesheet" type="text/css" href="resources/fontawesome-free-6.5.1-web/css/solid.css">
-<link rel="stylesheet" type="text/css" href="resources/sidebar.css">
+<link rel="stylesheet" type="text/css" href="resources/css/sidebar.css">
 <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha384-vtXRMe3mGCbOeY7l30aIg8H9p3GdeSe4IFlP6G8JMa7o7lXvnz3GFKzPxzJdPfGK" crossorigin="anonymous"></script>
 
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
@@ -23,7 +23,7 @@
                     <div class="card-body">
                         <div class="row no-gutters align-items-center">
                             <div class="col mr-2">
-                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">View Checklist</div>
+                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1"><strong>View Checklist</strong></div>
 
                             </div>
                             <div class="col-auto">
@@ -41,11 +41,11 @@
                     <div class="card-body">
                         <div class="row no-gutters align-items-center">
                             <div class="col mr-2">
-                                <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Create Record</div>
+                                <div class="text-xs font-weight-bold text-success text-uppercase mb-1"><strong>Create Record</strong></div>
 
                             </div>
                             <div class="col-auto">
-                                <i class="fa-solid fa-book-medical fa-2x text-gray-300"></i>
+                            <i class="fa-solid fa-file-pen  fa-2x text-gray-300"></i>
                             </div>
                         </div>
                     </div>
@@ -59,7 +59,7 @@
                     <div class="card-body">
                         <div class="row no-gutters align-items-center">
                             <div class="col mr-2">
-                                <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Add Questions</div>
+                                <div class="text-xs font-weight-bold text-info text-uppercase mb-1"><strong>Add Questions</strong></div>
                                 <div class="row no-gutters align-items-center">
                                     <div class="col-auto">
 
@@ -83,8 +83,7 @@
                     <div class="card-body">
                         <div class="row no-gutters align-items-center">
                             <div class="col mr-2">
-                                <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Edit Question
-                                </div>
+                                <div class="text-xs font-weight-bold text-warning text-uppercase mb-1"><strong>Edit Question</strong></div>
 
                             </div>
                             <div class="col-auto">
@@ -104,12 +103,11 @@
                     <div class="card-body">
                         <div class="row no-gutters align-items-center">
                             <div class="col mr-2">
-                                <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">Update Records
-                                </div>
+                                <div class="text-xs font-weight-bold text-danger text-uppercase mb-1"><strong>Update Records</strong></div>
 
                             </div>
                             <div class="col-auto">
-                                <i class="fa-solid fa-file-pen  fa-2x text-gray-300"></i>
+                            <i class="fa-solid fa-book-medical fa-2x text-gray-300"></i>
 
                             </div>
                         </div>
