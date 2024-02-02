62bf5c10a5516c06154a21973470c7e46d747e8d
Mary Hope Tabunlupa <115769573+Symphonyxia@users.noreply.github.com>
2024-02-02 11:12:53 +0800

    Update header.php

--- header.php
+++ header.php
@@ -15,6 +15,9 @@ require_once('boot.php');
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
+    <link rel="stylesheet" href="resources/css/edit.css">
+    <link rel="stylesheet" href="resources/css/view.css">
+
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha384-vtXRMe3mGCbOeY7l30aIg8H9p3GdeSe4IFlP6G8JMa7o7lXvnz3GFKzPxzJdPfGK" crossorigin="anonymous"></script>
 
@@ -22,9 +25,10 @@ require_once('boot.php');
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
     <script src="resources/js/table.js"></script>
-
     <script src="resources/js/update_questions.js"></script>
     <script src="resources/js/create.js"></script>
+    <script src="resources/js/edit_question.js"></script>
+
 
 
 
@@ -57,23 +61,23 @@ require_once('boot.php');
 </header>
 
 <body>
-    <?php
-    if (isset($_SESSION['error'])) {
-        echo "<div class='alert alert-danger text-center'>
+<?php
+if (isset($_SESSION['error'])) {
+    echo "<div class='alert alert-danger text-center'>
             <i class='fas fa-exclamation-triangle'></i> " . $_SESSION['error'] . "
           </div>";
 
-        // unset error
-        unset($_SESSION['error']);
-    }
+    // unset error
+    unset($_SESSION['error']);
+}
 
-    if (isset($_SESSION['success'])) {
-        echo "<div class='alert alert-success text-center'>
+if (isset($_SESSION['success'])) {
+    echo "<div class='alert alert-success text-center'>
             <i class='fas fa-check-circle'></i> " . $_SESSION['success'] . "
           </div>";
 
-        // unset success
-        unset($_SESSION['success']);
-    }
-    ?>
+    // unset success
+    unset($_SESSION['success']);
+}
+?>
     <br>
