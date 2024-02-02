ab978e3bcd9911a00cb4bc6e3572aef64e6234b1
Mary Hope Tabunlupa <115769573+Symphonyxia@users.noreply.github.com>
2024-02-02 11:07:44 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include 'boot.php';
 ?>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
-<link rel="stylesheet" type="text/css" href="resources/style.css">
+<link rel="stylesheet" type="text/css" href="resources/css/style.css">
 
 <div class="container">
   <h1 class="text-center" style="margin-top:30px;">Login Page</h1>
