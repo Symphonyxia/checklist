8d0d99ebfe6abae140c614236fecbdfb6718604d
Mary Hope Tabunlupa <115769573+Symphonyxia@users.noreply.github.com>
2024-02-05 08:04:42 +0800

    Update search.php

--- resources/dr/search.php
+++ resources/dr/search.php
@@ -8,29 +8,24 @@ if (isset($_POST['deleteQuestion'])) {
     try {
         $pdo->beginTransaction();
 
-        // Delete related records from checklist_result table
         $deleteChecklistResultStmt = $pdo->prepare('DELETE FROM checklist_result WHERE questions_id = ?');
         $deleteChecklistResultStmt->execute([$questionIdToDelete]);
 
-        // Delete the question from the questions table
         $deleteQuestionStmt = $pdo->prepare('DELETE FROM questions WHERE questions_id = ?');
         $deleteQuestionStmt->execute([$questionIdToDelete]);
 
         $pdo->commit();
 
-        // Send a JSON response to indicate success
         echo json_encode(['success' => true]);
         exit();
     } catch (PDOException $e) {
         $pdo->rollBack();
 
-        // Send a JSON response to indicate failure
         echo json_encode(['success' => false, 'message' => 'Failed to delete the question.']);
         exit();
     }
 }
 
-// Check if the form is submitted for updating
 if (isset($_POST['updateform'])) {
     $questionIds = $_POST['question_id'];
     $groups = isset($_POST['group']) ? $_POST['group'] : array();
@@ -41,7 +36,6 @@ if (isset($_POST['updateform'])) {
         $pdo->beginTransaction();
 
         foreach ($questionIds as $key => $questionId) {
-            // Update questions
             $updateStmt = $pdo->prepare('
                 UPDATE questions
                 SET group = :group, display_text = :display_text, max_points = :max_points
@@ -68,4 +62,4 @@ if (isset($_POST['updateform'])) {
     }
 }
 
-ob_end_flush(); // Flush the output buffer and send it
+ob_end_flush(); 
