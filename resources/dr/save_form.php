1fb74e49181b48c4d90cf3e9fb4a5adf1e774bdf
Mary Hope Tabunlupa <115769573+Symphonyxia@users.noreply.github.com>
2024-02-05 08:03:57 +0800

    Update save_form.php

--- resources/dr/save_form.php
+++ resources/dr/save_form.php
@@ -6,22 +6,18 @@ if (isset($_POST['addform'])) {
     $display_texts = $_POST['display_text'];
     $max_points = $_POST['max_points'];
 
-    // Ensure both arrays have the same length
     if (count($groups) !== count($display_texts)) {
         $_SESSION['error'] = 'Number of groups and display texts should match';
         header("Location: ../../create.php");
         exit();
     }
 
-    // Use prepared statements and parameter binding for security
     $insertQuestionStmt = $pdo->prepare('INSERT INTO questions (`group`, `display_text`, `max_points`) VALUES (:group, :display_text, :max_points)');
 
     try {
 
 
-        // Insert each question into the database and associate with the checklist
         for ($i = 0; $i < count($groups); $i++) {
-            // Insert into questions table
             $insertQuestionStmt->execute(['group' => $groups[$i], 'display_text' => $display_texts[$i], 'max_points' => $max_points[$i]]);
             $questionsId = $pdo->lastInsertId();
         }
